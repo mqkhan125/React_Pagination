a52cfa26c3f400ed47ea8f1994158ec5cf304b17
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-04-07 09:35:27 +0500

    feat: add image fetching from API

--- src/Component/Pagination.jsx
+++ src/Component/Pagination.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Pagination = () => {
+
+    const [images, setImages] = useState([])
+    const getImages = async() => {
+        const response = await fetch('https://picsum.photos/v2/list?page=1&limit=5')
+        const jsonData = await response.json()
+        setImages(jsonData)
+    }
+
+    useEffect(() => {
+       getImages()
+    },[])
+
+    
+
   return (
-    <div>Pagination</div>
-  )
+    <>
+      <div>
+        <div className="image-container">
+          {images.map((image, index) => (
+              <img key={index} src={image.download_url} alt={`Image ${index}`} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Pagination
