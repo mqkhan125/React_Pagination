b5c7bc01a155ce88866b87570df3538eca7d9784
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-04-07 10:05:04 +0500

    fix: correct API call and improve pagination button handling

--- src/Component/Pagination.jsx
+++ src/Component/Pagination.jsx
@@ -3,15 +3,17 @@ import React, { useEffect, useState } from 'react'
 const Pagination = () => {
 
     const [images, setImages] = useState([])
+    const [page, setPage] = useState(1)
     const getImages = async() => {
-        const response = await fetch('https://picsum.photos/v2/list?page=1&limit=5')
+        const response = await fetch(`https://picsum.photos/v2/list?page=${page}1&limit=5`)
         const jsonData = await response.json()
         setImages(jsonData)
     }
 
     useEffect(() => {
        getImages()
-    },[])
+    },[page])
+
 
     
 
@@ -20,8 +22,14 @@ const Pagination = () => {
       <div>
         <div className="image-container">
           {images.map((image, index) => (
-              <img key={index} src={image.download_url} alt={`Image ${index}`} />
+            <img key={index} src={image.download_url} alt={`Image ${index}`} />
           ))}
+        </div>
+
+        <div className="buttons">
+          <button onClick={() => setPage(page - 1)} disabled= {page === 1}> Prev </button>
+          <button>{page}</button>
+          <button onClick={() => setPage(page + 1)}>Next</button>
         </div>
       </div>
     </>
